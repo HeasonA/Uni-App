7f1ceb103b240e3b5e8e785b07d16fd7e2bccaca
HeasonA <2905654838@qq.com>
2024-06-10 22:00:55 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+import App from './App'
+import store from './store';
+
+// const app = createApp(App);
+// app.use(store);
+// app.mount('#app');
+// #ifndef VUE3
+import Vue from 'vue'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+	...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import {
+	createSSRApp
+} from 'vue'
+export function createApp() {
+	const app = createSSRApp(App)
+	return {
+		app
+	}
+}
+// #endif
