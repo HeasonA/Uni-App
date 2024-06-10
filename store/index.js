674279554a690f348e89ac6a4150e99445b399d9
HeasonA <2905654838@qq.com>
2024-06-10 22:05:28 +0800

    Add files via upload

---
+++ store/index.js
@@ -0,0 +1,30 @@
+// store/index.js
+// store/index.js
+import {
+	createStore
+} from 'vuex';
+
+export default createStore({
+	state() {
+		return {
+			count: 0
+		};
+	},
+	mutations: {
+		increment(state) {
+			state.count++;
+		}
+	},
+	actions: {
+		increment({
+			commit
+		}) {
+			commit('increment');
+		}
+	},
+	getters: {
+		doubleCount(state) {
+			return state.count * 2;
+		}
+	}
+});
